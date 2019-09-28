5be9784e73930ea949e892abb280a85d79a7e142
Gleb Azeev <azeevg@gmail.com>
2019-09-28 18:49:04 +0300

    T-30: format file

--- packages/showcase/src/index.ts
+++ packages/showcase/src/index.ts
@@ -1,15 +1,37 @@
 import Koa from 'koa';
 import KoaRouter from 'koa-router';
+import bodyParser from 'koa-bodyparser';
 
 const app = new Koa();
 const router = new KoaRouter();
+
+const generateRandomString = (): string => {
+    return Math.random()
+        .toString(36)
+        .substring(2, 15);
+};
+
+const generateToken = (): string => {
+    return generateRandomString() + generateRandomString();
+};
 
 router.get('/', (ctx, next) => {
     ctx.body = 'Hello World!';
     next();
 });
 
-app.use(router.routes());
+router.post('/api/process', (ctx, next) => {
+    const text: string = ctx.request.body.text;
+    if (text && text.length > 0) {
+        const tokenString: string = generateToken();
+        ctx.body = { token: tokenString.toUpperCase() };
+    } else {
+        ctx.status = 422;
+    }
+    next();
+});
 
+app.use(bodyParser());
+app.use(router.routes());
 app.listen(3000);
 console.log('listening on 3000');
