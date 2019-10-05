ee9d02e729221301f9ed2ea1dc6836c53963eec6
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2019-10-06 01:12:23 +0300

    [T-10 T-7] Add log file to gitignore

--- packages/browser-extensions/src/popup/src/App.tsx
+++ packages/browser-extensions/src/popup/src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    ooooo22
+                    {"THE MAGIC WORDS ARE SQUEAMISH OSSIFRAGE"}
                 </a>
             </header>
         </div>
