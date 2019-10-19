b10ad96a87b716abe0361e8dab6acc97e6be0233
ledamint <heresliker@gmail.com>
2019-10-19 16:10:22 +0300

    #24 revert dev fixes

--- packages/browser-extensions/src/contentscript/contentscript.ts
+++ packages/browser-extensions/src/contentscript/contentscript.ts
@@ -7,27 +7,24 @@ document.addEventListener('DOMContentLoaded', async () => {
     const body = document.getElementsByTagName('body')[0];
     const text = parseMainContent(document, location.href);
 
-    console.log(123);
+    if (text.length === 0) {
+        return;
+    }
 
-    // if (text.length === 0) {
-    //     return;
-    // }
-    //
-    //
-    // const messenger = new Messenger();
-    // const response = await messenger.send<ReturnType<typeof extractResponse>>(extractRequest(text));
-    //
-    // if (!response || !response.payload) {
-    //     return;
-    // }
-    //
-    // const { FAC, EVENT, GPE, PERSON, PRODUCT, LOC, ORG } = response.payload;
-    //
-    // FAC && colorizeEntities(body, FAC, 'peru');
-    // EVENT && colorizeEntities(body, EVENT, 'yellow');
-    // GPE && colorizeEntities(body, GPE, 'violet');
-    // PERSON && colorizeEntities(body, PERSON, 'magenta');
-    // PRODUCT && colorizeEntities(body, PRODUCT, 'skyblue');
-    // LOC && colorizeEntities(body, LOC, 'silver');
-    // ORG && colorizeEntities(body, ORG, 'mediumorchid');
+    const messenger = new Messenger();
+    const response = await messenger.send<ReturnType<typeof extractResponse>>(extractRequest(text));
+
+    if (!response || !response.payload) {
+        return;
+    }
+
+    const { FAC, EVENT, GPE, PERSON, PRODUCT, LOC, ORG } = response.payload;
+
+    FAC && colorizeEntities(body, FAC, 'peru');
+    EVENT && colorizeEntities(body, EVENT, 'yellow');
+    GPE && colorizeEntities(body, GPE, 'violet');
+    PERSON && colorizeEntities(body, PERSON, 'magenta');
+    PRODUCT && colorizeEntities(body, PRODUCT, 'skyblue');
+    LOC && colorizeEntities(body, LOC, 'silver');
+    ORG && colorizeEntities(body, ORG, 'mediumorchid');
 });
