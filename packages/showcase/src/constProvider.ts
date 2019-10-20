7b181bb4d25c3676757d73caf8382cea95904f7b
Gleb Azeev <azeevg@gmail.com>
2019-10-20 21:40:02 +0300

    #16 simplify returned type

--- packages/showcase/src/constProvider.ts
+++ packages/showcase/src/constProvider.ts
@@ -2,6 +2,6 @@ export const getNer = (): string | undefined => {
     return process.env.NER_API;
 };
 
-export const getAppPort = (): number | string => {
-    return process.env.APP_PORT || 3000;
+export const getAppPort = (): string => {
+    return process.env.APP_PORT || "3000";
 };
