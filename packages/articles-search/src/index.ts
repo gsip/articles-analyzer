fcbe5ed82462de3379d2d2cea9c97627e024ed06
ledamint <heresliker@gmail.com>
2019-11-15 19:48:05 +0300

    #75 fix url and add multiple articles meta

--- packages/articles-search/src/index.ts
+++ packages/articles-search/src/index.ts
@@ -1,35 +1,50 @@
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
+import { startFrom } from './utils';
 
-type Response = {
+export type ArticleMeta = {
     title: string;
     url: string;
     summary: string;
 };
 
-export const getRequest = async (queries: string[], site?: string): Promise<Response> => {
-    let query = queries
-        // .map((q) => `"${q}"`)
-        .join(' ');
-    query = site ? `site:${site} ${queries}` : query;
-
-    const res = await fetch(`https://duckduckgo.com/html/?q=${query}`);
-    const html = await res.text();
-    const $ = cheerio.load(html);
-
-    const titles = $('.result__title');
-    const urls = $('.result__url');
-    const summaries = $('.result__snippet');
-
-    return {
-        title: $(titles[0])
-            .text()
-            .trim(),
-        url: $(urls[0])
-            .attr('href')
-            .trim(),
-        summary: $(summaries[0]).text(),
-    };
+const getText = ($element: Cheerio): string => {
+    return $element.text().trim();
 };
 
-(async () => console.log(await getRequest(['javascript', 'vue', 'angular'])))();
+const getUrl = ($element: Cheerio): string => {
+    const url = $element.attr('href').trim();
+
+    return decodeURIComponent(startFrom(url, 'http'));
+};
+
+export const getArticlesMeta = async (queries: string[], site?: string, count = 3): Promise<ArticleMeta[]> => {
+    try {
+        let query = queries
+            // .map((q) => `"${q}"`)
+            .join(' ');
+        query = site ? `site:${site} ${queries}` : query;
+
+        const res = await fetch(`https://duckduckgo.com/html/?q=${query} !safeon`);
+        const html = await res.text();
+        const $ = cheerio.load(html);
+
+        const titles = $('.result__title');
+        const urls = $('.result__url');
+        const summaries = $('.result__snippet');
+        const result = [];
+
+        for (let i = 0; i < count; i++) {
+            const title = getText($(titles[i]));
+            const url = getUrl($(urls[i]));
+            const summary = getText($(summaries[i]));
+
+            result.push({ title, url, summary });
+        }
+
+        return result;
+    } catch (e) {
+        console.error(e);
+        return [];
+    }
+};
