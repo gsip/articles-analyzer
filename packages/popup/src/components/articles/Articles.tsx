97eccdb110b11334b5c25bd84d6a14404f3a8533
ledamint <heresliker@gmail.com>
2019-11-16 15:02:52 +0300

    #75 show full info

--- packages/popup/src/components/articles/Articles.tsx
+++ packages/popup/src/components/articles/Articles.tsx
@@ -8,22 +8,38 @@ type Props = {
 
 export function Articles({ words }: Props): React.ReactElement {
     const [articlesMeta, setArticlesMeta] = useState<ArticleMeta[]>([]);
-    const sortedWords = useMemo(() => {
-        return words.sort((a, b) => (a.count < b.count ? 1 : -1));
+    const mainKeywords = useMemo(() => {
+        const mainKeywords = words
+            .filter(({ count }) => count > 2)
+            .sort((a, b) => (a.count < b.count ? 1 : -1))
+            .map(({ word }) => word);
+
+        return [...new Set(mainKeywords)].slice(0, 4);
     }, [words]);
 
     useEffect(() => {
+        // console.log(words.filter(({ count }) => count > 2).sort((a, b) => (a.count < b.count ? 1 : -1)));
+        // console.log(mainKeywords);
         // FIXME [AS-75] move getArticlesMeta to background script?
-        getArticlesMeta(sortedWords.map((word) => word.word).slice(0, 3)).then((articlesMeta) =>
-            setArticlesMeta(articlesMeta),
-        );
-    }, [sortedWords]);
+        getArticlesMeta(mainKeywords, 'nytimes.com').then(setArticlesMeta);
+    }, [mainKeywords]);
 
     return (
-        <div>
-            {articlesMeta.map(({ title }) => (
-                <div key={title}>{title}</div>
-            ))}
+        <div className="articles">
+            <h3>See also</h3>
+            <div>
+                {articlesMeta.map(({ title, url, summary }) => (
+                    <div key={title}>
+                        <a href={url} target="_blank" rel="noopener noreferrer">
+                            <div>
+                                <h4>{title}</h4>
+                                <span>{new URL(url).host}</span>
+                                <div className="summary">{summary}</div>
+                            </div>
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
