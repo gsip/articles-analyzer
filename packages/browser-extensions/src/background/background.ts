71be70efc699c4ccabb0bad8e2c12975bca4002a
ledamint <heresliker@gmail.com>
2019-11-17 22:20:50 +0300

    #75 add memoization

--- packages/browser-extensions/src/background/background.ts
+++ packages/browser-extensions/src/background/background.ts
@@ -1,17 +1,8 @@
 import { getNER, getSummary } from '../modules/api/extract';
 import { messenger } from '@reservoir-dogs/browser-transport';
-import {
-    extractRequest,
-    ExtractType,
-    ContentPageEvent,
-    PopupPageEvent,
-    keywordHover,
-    wantToGetSimilarArticles,
-    getMainKeywords,
-} from '@reservoir-dogs/model';
+import { extractRequest, ExtractType, ContentPageEvent, keywordHover } from '@reservoir-dogs/model';
 import { getWikiSummary } from '../modules/wikipedia';
-import { getArticlesMeta } from '@reservoir-dogs/articles-search';
-import { ArticleMeta } from '@reservoir-dogs/articles-search';
+import { initializeWantToGetSimilarArticles } from './controllers/wantToGetSimilarArticles';
 
 messenger.subscribe(ExtractType.EXTRACT_REQUEST, async (action: ReturnType<typeof extractRequest>) => {
     const [ner, summary] = await Promise.all([getNER(action.payload), getSummary(action.payload)]);
@@ -25,28 +16,4 @@ messenger.subscribe(ContentPageEvent.KEYWORD_HOVER, async (action: ReturnType<ty
     return summary;
 });
 
-messenger.subscribe(
-    PopupPageEvent.WANT_TO_GET_SIMILAR_ARTICLES,
-    (action: ReturnType<typeof wantToGetSimilarArticles>): Promise<ArticleMeta[]> => {
-        return new Promise((resolve) => {
-            chrome.tabs.getSelected(async (tab) => {
-                if (!tab || !tab.url) {
-                    console.error('Can not find active tab url');
-
-                    return [];
-                }
-                const mainKeywords = getMainKeywords(action.payload);
-                const urlObject = new URL(tab.url);
-                const articlesMeta = await getArticlesMeta(mainKeywords, urlObject.hostname, 4);
-
-                const filteredArticlesMeta = articlesMeta
-                    .filter((articleMeta) => {
-                        return !articleMeta.url.includes(urlObject.origin + urlObject.pathname);
-                    })
-                    .slice(0, 3);
-
-                resolve(filteredArticlesMeta);
-            });
-        });
-    },
-);
+initializeWantToGetSimilarArticles();
---
+++ packages/browser-extensions/src/background/controllers/wantToGetSimilarArticles.ts
@@ -0,0 +1,48 @@
+import { messenger } from '@reservoir-dogs/browser-transport';
+import { PopupPageEvent, wantToGetSimilarArticles, getMainKeywords, NEREntities } from '@reservoir-dogs/model';
+import { getArticlesMeta } from '@reservoir-dogs/articles-search';
+import { ArticleMeta } from '@reservoir-dogs/articles-search';
+import { memoize } from 'lodash-es';
+
+const getSelectedTab = (): Promise<chrome.tabs.Tab | null> => {
+    return new Promise((resolve) => {
+        chrome.tabs.getSelected(async (tab) => {
+            if (!tab || !tab.url) {
+                resolve(null);
+            }
+
+            resolve(tab);
+        });
+    });
+};
+
+const callback = memoize(
+    async (url: string, nerEntities: NEREntities): Promise<ArticleMeta[]> => {
+        const mainKeywords = getMainKeywords(nerEntities);
+        const urlObject = new URL(url);
+        const articlesMeta = await getArticlesMeta(mainKeywords, urlObject.hostname, 4);
+
+        return articlesMeta
+            .filter((articleMeta) => {
+                return !articleMeta.url.includes(urlObject.origin + urlObject.pathname);
+            })
+            .slice(0, 3);
+    },
+);
+
+export const initializeWantToGetSimilarArticles = (): void => {
+    messenger.subscribe(
+        PopupPageEvent.WANT_TO_GET_SIMILAR_ARTICLES,
+        async (action: ReturnType<typeof wantToGetSimilarArticles>): Promise<ArticleMeta[]> => {
+            const tab = await getSelectedTab();
+
+            if (!tab || !tab.url) {
+                console.error('Can not find active tab url');
+
+                return [];
+            }
+
+            return callback(tab.url, action.payload);
+        },
+    );
+};
