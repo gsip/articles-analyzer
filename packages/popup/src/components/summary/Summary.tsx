6249cb9e8f04cefaf55002d43ae614694d1221a7
ledamint <heresliker@gmail.com>
2019-11-25 16:00:17 +0300

    fix warning

--- packages/popup/src/components/summary/Summary.tsx
+++ packages/popup/src/components/summary/Summary.tsx
@@ -8,7 +8,7 @@ export function Summary({ summary }: Props): React.ReactElement {
     return (
         <>
             <h3 className="title">Summary</h3>
-            <p className="summary">
+            <div className="summary">
                 {summary
                     .split('\\n')
                     .filter(Boolean)
@@ -19,7 +19,7 @@ export function Summary({ summary }: Props): React.ReactElement {
                             </div>
                         );
                     })}
-            </p>
+            </div>
         </>
     );
 }
