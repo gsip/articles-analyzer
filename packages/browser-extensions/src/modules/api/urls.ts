01adb0ddfae43f3bf7be24b91b3583f483b2c27a
ledamint <heresliker@gmail.com>
2020-01-02 14:15:38 +0300

    #55 fix build and urls

--- packages/browser-extensions/src/modules/api/urls.ts
+++ packages/browser-extensions/src/modules/api/urls.ts
@@ -1,8 +1,4 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const NER_API_URL = isDevelopment
-    ? 'http://localhost:5001/extract'
-    : 'https://us-central1-articles-summary-239.cloudfunctions.net/ner';
-export const SUMMARY_API_URL = isDevelopment
-    ? 'http://localhost:5002/sum'
-    : 'https://us-central1-articles-summary-239.cloudfunctions.net/sum';
+export const NER_API_URL = isDevelopment ? 'http://localhost:5001/extract' : 'https://rd-ner.herokuapp.com/extract';
+export const SUMMARY_API_URL = isDevelopment ? 'http://localhost:5002/sum' : 'https://rd-summ.herokuapp.com/sum';
