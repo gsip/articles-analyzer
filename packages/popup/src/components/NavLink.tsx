6318ad91a9bd8c8450863d07b9427fe8c7f32763
ledamint <heresliker@gmail.com>
2020-01-04 14:50:50 +0300

    add useCallback

--- packages/popup/src/components/NavLink.tsx
+++ packages/popup/src/components/NavLink.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from '@reach/router';
 
-export const NavLink = (props: any): React.ReactElement => (
-    <Link
-        onClick={() => {
-            localStorage.setItem('path', props.to);
-        }}
-        {...props}
-        getProps={({ isCurrent }) => {
-            return {
-                className: isCurrent ? 'nav-link active' : 'nav-link',
-            };
-        }}
-    />
-);
+export const NavLink = (props: any): React.ReactElement => {
+    const onClick = useCallback(() => {
+        localStorage.setItem('path', props.to);
+    }, [props.to]);
+    const getProps = useCallback(({ isCurrent }) => ({ className: isCurrent ? 'nav-link active' : 'nav-link' }), []);
+
+    return <Link onClick={onClick} {...props} getProps={getProps} />;
+};
