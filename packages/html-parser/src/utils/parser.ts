8ab745501e4af66e3cb44d97c88201d33fb854ad
oTkPoBeHuE <xxoTkPoBeHuExx@gmail.com>
2020-01-04 15:44:33 +0300

    #97 By default, heading are uppercased. Fixed.

--- packages/html-parser/src/utils/parser.ts
+++ packages/html-parser/src/utils/parser.ts
@@ -14,6 +14,7 @@ export const getHTMLStringContent = (outerHTML: string, options: HtmlToTextOptio
             ignoreHref: true,
             ignoreImage: true,
             preserveNewlines: true,
+            uppercaseHeadings: false,
             ...options,
         }) || '';
 
